2c74ec577a5eaaa3503bf757b237952bc7192727
bwalker <brent@moveablecube.com>
2016-04-16 07:25:23 -0400

    expand tests to cover other attributes

--- spec/unit/recipes/worker_spec.rb
+++ spec/unit/recipes/worker_spec.rb
@@ -59,4 +59,68 @@ describe 'swarm::worker' do
       )
     end
   end
+
+  context 'When specifying heartbeat and ttl' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new do |node|
+        node.automatic['ipaddress'] = '10.1.1.1'
+        node.set['swarm']['discovery']['token'] = 'fba21ffa43f1e13ef799d293eb74979a'
+
+        node.set['swarm']['worker']['heartbeat'] = '60s'
+        node.set['swarm']['worker']['ttl'] = '120s'
+      end
+
+      runner.converge(described_recipe)
+    end
+
+    it 'runs the swarm worker container' do
+      expect(chef_run).to run_docker_container('swarm-worker').with(
+        tag: 'latest',
+        restart_policy: 'on-failure',
+        command: 'join --advertise 10.1.1.1:2376 --heartbeat 60s --ttl 120s token://fba21ffa43f1e13ef799d293eb74979a'
+      )
+    end
+  end
+
+  context 'When specifying discovery options' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new do |node|
+        node.automatic['ipaddress'] = '10.1.1.1'
+        node.set['swarm']['discovery']['token'] = 'fba21ffa43f1e13ef799d293eb74979a'
+
+        node.set['swarm']['discovery_options'] = ['option1', 'option2']
+      end
+
+      runner.converge(described_recipe)
+    end
+
+    it 'runs the swarm worker container' do
+      expect(chef_run).to run_docker_container('swarm-worker').with(
+        tag: 'latest',
+        restart_policy: 'on-failure',
+        command: 'join --advertise 10.1.1.1:2376 --discovery-opt option1 --discovery-opt option2 token://fba21ffa43f1e13ef799d293eb74979a'
+      )
+    end
+  end
+
+  context 'When specifying additional options' do
+    let(:chef_run) do
+      runner = ChefSpec::SoloRunner.new do |node|
+        node.automatic['ipaddress'] = '10.1.1.1'
+        node.set['swarm']['discovery']['token'] = 'fba21ffa43f1e13ef799d293eb74979a'
+
+        node.set['swarm']['worker']['additional_options'] = { delay: '1s' }
+      end
+
+      runner.converge(described_recipe)
+    end
+
+    it 'runs the swarm worker container' do
+      expect(chef_run).to run_docker_container('swarm-worker').with(
+        tag: 'latest',
+        restart_policy: 'on-failure',
+        command: 'join --advertise 10.1.1.1:2376 --delay 1s token://fba21ffa43f1e13ef799d293eb74979a'
+      )
+    end
+  end
 end
